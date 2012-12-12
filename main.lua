ec6ed0e27600149e10695300ff07f1893a6948fe
Mateusz CzapliĹ„ski <czapkofan@gmail.com>
2012-12-13 00:16:42 +0100

    extended warning about Windoze-only support

--- main.lua
+++ main.lua
@@ -1,11 +1,16 @@
 local ffi = require 'ffi'
 local C = ffi.C
 
-if ffi.os ~= 'Windows' then
-	print "Only Windows OS is currently supported..."
+local crt = require 'crt-win32'
+
+if not crt then
+	print "We plea thy forgiveness, only Windows OS currently supported..."
+	print ""
+	print "But have a look at 'crt-win32.lua'. It's really dumb, you totally"
+	print "should be able to hack something similarly primitive for this"
+	print "Ohsogreat Platform of yours."
 	return
 end
-local crt = require 'crt-win32'
 
 crt.gotoxy(5, 3)
 print('hello')
